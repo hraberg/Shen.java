4a01890ba4584d65d94b0c8c74ebda7718dac4e3
Hakan Raberg <hraberg@yahoo.se>
2013-01-21 17:23:57 +0000

    Making the tests reflects that ints are long gone to long

--- test/shen/PrimitivesTest.java
+++ test/shen/PrimitivesTest.java
@@ -23,7 +23,7 @@ import static shen.Shen.Primitives.intern;
 public class PrimitivesTest {
     @Test
     public void equals() {
-        is(2, "2");
+        is(2L, "2");
         is(true, "true");
         is("foo", "\"foo\"");
         is(intern("bar"), "bar");
@@ -36,36 +36,36 @@ public class PrimitivesTest {
     @Test
     public void defun_lambda_and_let() {
         is(intern("f"), "(defun f (x) (lambda y (+ x y))");
-        is(5, "((f 3) 2)");
-        is(10, "(let x 5 (* 2 x)");
+        is(5L, "((f 3) 2)");
+        is(10L, "(let x 5 (* 2 x)");
     }
 
     @Test
     public void errors() {
         is(1.0, "(/ 1 1)");
-        is(-1, "(trap-error (/ 1 0) (lambda E -1))");
+        is(-1L, "(trap-error (/ 1 0) (lambda E -1))");
         is(1.0, "(trap-error (/ 1 1) (lambda E -1))");
-        is(-1, "(if (trap-error (/ 1 0) (lambda E true)) -1 false)");
-        is(-1, "(if (trap-error (= 1 1) (lambda E false)) -1 false)");
+        is(-1L, "(if (trap-error (/ 1 0) (lambda E true)) -1 false)");
+        is(-1L, "(if (trap-error (= 1 1) (lambda E false)) -1 false)");
         is("testError", "(trap-error (set newError (simple-error \"testError\")) (lambda E (error-to-string E)))");
     }
 
     @Test
     public void addition() {
-        is(5, "(+ 2 3)");
-        is(-1, "(+ 2 -3)");
+        is(5L, "(+ 2 3)");
+        is(-1L, "(+ 2 -3)");
     }
 
     @Test
     public void subtraction() {
-        is(-1, "(- 2 3)");
-        is(5, "(- 2 -3)");
+        is(-1L, "(- 2 3)");
+        is(5L, "(- 2 -3)");
     }
 
     @Test
     public void multiplication() {
-        is(6, "(* 2 3)");
-        is(-6, "(* 2 -3)");
+        is(6L, "(* 2 3)");
+        is(-6L, "(* 2 -3)");
     }
 
     @Test
@@ -106,7 +106,7 @@ public class PrimitivesTest {
     public void _if() {
         is(true, "(if true true false)");
         is(true, "(if false false true)");
-        is(-1, "(trap-error (if 5.0 true true) (lambda _ -1)");
+        is(-1L, "(trap-error (if 5.0 true true) (lambda _ -1)");
     }
 
     @Test
@@ -121,7 +121,7 @@ public class PrimitivesTest {
     public void and() {
         is(true, "(and true true true)");
         is(false, "(and true false true)");
-        is(-1, "(trap-error (and 2 true) (lambda E -1)");
+        is(-1L, "(trap-error (and 2 true) (lambda E -1)");
     }
 
     @Test
@@ -129,16 +129,16 @@ public class PrimitivesTest {
         is(true, "(or true true true)");
         is(true, "(or true false true)");
         is(true, "(or (and false false false) true)");
-        is(-1, "(trap-error (or 2 true) (lambda E -1)");
+        is(-1L, "(trap-error (or 2 true) (lambda E -1)");
     }
 
     @Test
     public void value() {
-        is(5, "(set x 5)");
-        is(5, "(value x)");
+        is(5L, "(set x 5)");
+        is(5L, "(value x)");
         is("foo", "(set x \"foo\")");
         is("foo", "(value x)");
-        is(-1, "(trap-error (value valueTest) (lambda E -1)");
+        is(-1L, "(trap-error (value valueTest) (lambda E -1)");
     }
 
     @Test
@@ -151,7 +151,7 @@ public class PrimitivesTest {
         is("bar", "(str bar)");
         is(false, "(string? 55)");
         is("oobar", "(tlstr \"foobar\")");
-        is(-1, "(trap-error (cn bla blub) (lambda _ -1)");
+        is(-1L, "(trap-error (cn bla blub) (lambda _ -1)");
         is("foobar", "(cn \"foo\" \"bar\")");
         is("r", "(pos \"bar\" 2)");
     }
@@ -161,8 +161,8 @@ public class PrimitivesTest {
         is(true, "(cons? (cons 2 (cons 1 (cons 1 (cons \"foo\" ())))))");
         is(true, "(cons? (cons 3 (cons 2 (cons 1 (cons 1 (cons \"foo\" ()))))))");
         is(false, "(cons? 53)");
-        is(2, "(hd (cons 2 (cons 1 (cons 1 ()))))");
-        is(1, "(hd (tl (cons 2 (cons 1 (cons 1 ())))))");
+        is(2L, "(hd (cons 2 (cons 1 (cons 1 ()))))");
+        is(1L, "(hd (tl (cons 2 (cons 1 (cons 1 ())))))");
     }
 
     @Test
@@ -175,37 +175,37 @@ public class PrimitivesTest {
         is(false, "(absvector? \"foo\")");
         absvector[2] = 5L;
         is(absvector, "(address-> (value v) 2 5)");
-        is(5, "(<-address (value v) 2)");
-        is(-1, "(trap-error (<-address (value v) 5) (lambda E -1))");
+        is(5L, "(<-address (value v) 2)");
+        is(-1L, "(trap-error (<-address (value v) 5) (lambda E -1))");
     }
 
     @Test
     public void eval_kl_freeze_and_thaw() {
-        is(9, "(eval-kl (cons + (cons 4 (cons 5 ()))))");
-        is(4, "(eval-kl 4)");
+        is(9L, "(eval-kl (cons + (cons 4 (cons 5 ()))))");
+        is(4L, "(eval-kl 4)");
         is(intern("hello"), "(eval-kl hello)");
         is(intern("hello"), "(eval-kl hello)");
         is(MethodHandle.class, "(freeze (+ 2 2)");
         is(MethodHandle.class, "(freeze (/ 2 0))");
-        is(4, "((freeze (+ 2 2)))");
-        is(4, "(thaw (freeze (+ 2 2)))");
+        is(4L, "((freeze (+ 2 2)))");
+        is(4L, "(thaw (freeze (+ 2 2)))");
     }
 
     @Test
     public void set_value_and_intern() {
-        is(5, "(set x 5)");
-        is(5, "(value x)");
-        is(5, "(value (intern \"x\")");
+        is(5L, "(set x 5)");
+        is(5L, "(value x)");
+        is(5L, "(value (intern \"x\")");
         is(intern("fun"), "(defun fun () (value x))");
-        is(5, "(fun)");
-        is(6, "(set x 6)");
-        is(6, "(fun)");
+        is(5L, "(fun)");
+        is(6L, "(set x 6)");
+        is(6L, "(fun)");
     }
 
     @Test
     public void tagged_values() {
-        is(5, "(set x 5)");
-        is(5, "(value x)");
+        is(5L, "(set x 5)");
+        is(5L, "(value x)");
         is(5.0, "(set x 5.0)");
         is(5.0, "(value x)");
         is(true, "(set x true)");
@@ -218,10 +218,10 @@ public class PrimitivesTest {
         is(asList(), "(fun x)");
         is(5.0, "(set x 5.0)");
         is(5.0, "(fun x)");
-        is(5, "(set x 5)");
-        is(5, "(fun x)");
-        is(6, "(set y 6)");
-        is(6, "(fun y)");
+        is(5L, "(set x 5)");
+        is(5L, "(fun x)");
+        is(6L, "(set y 6)");
+        is(6L, "(fun y)");
         is(asList(), "(set y ())");
         is(asList(), "(fun y)");
     }
@@ -238,8 +238,8 @@ public class PrimitivesTest {
         is("foobar", "(pr \"foobar\" (value writeFile))");
         is(asList(), "(close (value writeFile))");
         is(InputStream.class, "(set readFile (open file (value fileName) in))");
-        is(102, "(read-byte (value readFile))");
-        is(111, "(read-byte (value readFile))");
+        is(102L, "(read-byte (value readFile))");
+        is(111L, "(read-byte (value readFile))");
         is(false, "(= 102 (read-byte (value readFile)))");
         is(asList(), "(close (value readFile))");
     }
@@ -260,33 +260,33 @@ public class PrimitivesTest {
         is("h", "(n->string 104)");
         is("(", "(n->string 40)");
         is(false, "(= \"d\" (n->string 101)");
-        is(-1, "(trap-error (n->string -10) (lambda E -1)");
+        is(-1L, "(trap-error (n->string -10) (lambda E -1)");
     }
 
     @Test
     public void string_gt_n() {
-        is(100, "(string->n \"d\")");
-        is(104, "(string->n \"h\")");
-        is(40, "(string->n \"(\")");
+        is(100L, "(string->n \"d\")");
+        is(104L, "(string->n \"h\")");
+        is(40L, "(string->n \"(\")");
         is(false, "(= 101 (string->n \"d\")");
-        is(-1, "(trap-error (string-> \"\") (lambda E -1)");
+        is(-1L, "(trap-error (string-> \"\") (lambda E -1)");
     }
 
     @Test
     public void special_tests() {
         is(EMPTY_LIST, "()");
-        is(-1, "(trap-error ((4 3 2)) (lambda E -1))");
-        is(-1, "(trap-error (+4 2) (lambda E -1))");
-        is(-1, "(trap-error (+ 4 \"2\") (lambda E -1))");
-        is(-1, "(trap-error (+ 4 specialTest) (lambda E -1))");
-        is(-1, "(trap-error (+ 4 true) (lambda E -1))");
+        is(-1L, "(trap-error ((4 3 2)) (lambda E -1))");
+        is(-1L, "(trap-error (+4 2) (lambda E -1))");
+        is(-1L, "(trap-error (+ 4 \"2\") (lambda E -1))");
+        is(-1L, "(trap-error (+ 4 specialTest) (lambda E -1))");
+        is(-1L, "(trap-error (+ 4 true) (lambda E -1))");
     }
 
     @Test
     public void lists() {
-        is(-1, "(trap-error (hd 5) (lambda E -1))");
-        is(-1, "(trap-error (tl 5) (lambda E -1))");
-        is(1, "(hd (cons 1 (cons 2 (cons 3 ()))))");
+        is(-1L, "(trap-error (hd 5) (lambda E -1))");
+        is(-1L, "(trap-error (tl 5) (lambda E -1))");
+        is(1L, "(hd (cons 1 (cons 2 (cons 3 ()))))");
         is(asList(2L, 3L), "(tl (cons 1 (cons 2 (cons 3 ()))))");
         is(new Cons(5L, 10L), "(cons 5 10)");
     }
@@ -337,17 +337,17 @@ public class PrimitivesTest {
 
     @Test
     public void function() {
-        is(3, "((function +) 1 2)");
+        is(3L, "((function +) 1 2)");
         is(3.0, "((function +) 1 2.0)");
         is(intern("x"), "(defun x () y)");
         is(intern("y"), "(let z x (z)))");
         is(intern("x"), "(defun x (y) y)");
-        is(2, "(let a x (a 2)))");
+        is(2L, "(let a x (a 2)))");
         is(intern("x"), "(defun x (y) (y))");
         is(intern("y"), "(defun y () 1))");
-        is(1, "(x y)");
+        is(1L, "(x y)");
         is(MethodHandle.class, "(function undefined)");
-        is(-1, "(trap-error ((function undefined)) (lambda E -1))");
+        is(-1L, "(trap-error ((function undefined)) (lambda E -1))");
     }
 
     @Test
@@ -356,26 +356,26 @@ public class PrimitivesTest {
         is(asList(1L), "(fun ())");
         is(new Cons(1L, 2L), "(fun 2)");
         is(intern("fun2"), "(defun fun2 (x) (+ 2 x))");
-        is(3, "(fun2 1)");
+        is(3L, "(fun2 1)");
         is(3.0, "(fun2 1.0)");
     }
 
     @Test
     public void recur() {
         is(intern("factorial"), "(defun factorial (cnt acc) (if (= 0 cnt) acc (factorial (- cnt 1) (* acc cnt)))");
-        is(3628800, "(factorial 10 1)");
+        is(3628800L, "(factorial 10 1)");
     }
 
     @Test
     public void can_only_recur_from_tail_position() {
         is(intern("fib"), "(defun fib (n) (if (<= n 1) n (+ (fib (- n 1)) (fib (- n 2)))))");
-        is(55, "(fib 10)");
+        is(55L, "(fib 10)");
     }
 
     @Test
     public void kl_do() {
-        is(2, "(do 1 2)");
-        is(3, "(do 1 2 3)");
+        is(2L, "(do 1 2)");
+        is(3L, "(do 1 2 3)");
     }
 
     @Test
@@ -387,7 +387,7 @@ public class PrimitivesTest {
         is(asList(1L, 2L), "(Arrays/asList 1 2)");
         is(Class.class, "(import java.util.ArrayList)");
         is(Class.class, "(value ArrayList)");
-        is(0, "(.size ()))");
+        is(0L, "(.size ()))");
         is(ArrayList.class, "(ArrayList.)");
         is(asList(1L), "(ArrayList. (cons 1 ())");
         is(Long.class, "(.size (ArrayList. (cons 1 ()))");
@@ -417,32 +417,30 @@ public class PrimitivesTest {
         is("[1]", "(to-string (ArrayList. (cons 1 ()))");
         is("[1]", "(to-string (LinkedList. (cons 1 ()))");
         is(intern("size"), "(defun size (x) (.size x))");
-        is(1, "(size (ArrayList. (cons 1 ()))");
-        is(1, "(size (LinkedList. (cons 1 ()))");
-        is(0, "(size ())");
+        is(1L, "(size (ArrayList. (cons 1 ()))");
+        is(1L, "(size (LinkedList. (cons 1 ()))");
+        is(0L, "(size ())");
         is(Class.class, "(import java.util.BitSet)");
-        is(new BitSet().size(), "(size (BitSet.))");
+        is((long) new BitSet().size(), "(size (BitSet.))");
     }
 
     @Test
     public void redefine() {
         is(intern("fun"), "(defun fun (x y) (+ x y))");
         is(intern("fun2"), "(defun fun2 () (fun 1 2))");
-        is(3, "(fun 1 2)");
-        is(3, "(fun2)");
+        is(3L, "(fun 1 2)");
+        is(3L, "(fun2)");
         is(intern("fun"), "(defun fun (x y) (- x y))");
-        is(-1, "(fun 1 2)");
-        is(-1, "(fun2)");
+        is(-1L, "(fun 1 2)");
+        is(-1L, "(fun2)");
         is(intern("fun"), "(defun fun (x y) (+ x y))");
-        is(3, "(fun 1 2)");
-        is(3, "(fun2)");
+        is(3L, "(fun 1 2)");
+        is(3L, "(fun2)");
     }
 
     void is(Object expected, String actual) {
         if (expected instanceof Class)
             assertThat(神(actual), instanceOf((Class<?>) expected));
-        else if (expected instanceof Integer)
-            assertThat(((Number) 神(actual)).longValue(), equalTo(((Integer) expected).longValue()));
         else
             assertThat(神(actual), equalTo(expected));
     }
