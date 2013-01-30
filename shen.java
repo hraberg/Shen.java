f390e6253441f55d4705a91fbd5888aa8c525524
Hakan Raberg <hraberg@yahoo.se>
2013-01-30 01:00:25 +0100

    No need for shade/uberjar when depending on jdk.internal.org.objectweb.asm

--- shen.java
+++ shen.java
@@ -2,7 +2,7 @@
 
 rlwrap=$(which rlwrap) || "" &> /dev/null
 java="$rlwrap $JAVA_HOME/bin/java $JAVA_OPTS"
-shen="find . -name shen.java-*-standalone.jar"
+shen="find . -name shen.java-*.jar"
 
 test -z `$shen` && mvn package
 $java -jar `$shen`
